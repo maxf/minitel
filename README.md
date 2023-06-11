7e29bd472c45b88f7281830678dee9f2785902c6
Max Froumentin <github@e97.me>
2023-06-11 18:29:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Extra Notes
+===========
+
+
+Minitel used: Minitel 2, manufactured by Philips
+
+To put the minitel in terminal mode, press `Fnct`-`T` followed by `V`.
+
---
+++ max-test.py
@@ -0,0 +1,20 @@
+from minitel.Minitel import Minitel
+
+minitel = Minitel()
+
+vitesse = minitel.deviner_vitesse()
+print("vitesse", vitesse)
+
+if (vitesse == -1):
+    result = minitel.definir_vitesse(4800)
+    print('result', result)
+
+
+
+minitel.identifier()
+
+# ...
+# Utilisation de l’objet minitel
+# ...
+
+minitel.close()
