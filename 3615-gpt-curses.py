64cb28f8442bb393181911841647bcdc9c72cc28
Max Froumentin <max@e97.me>
2023-09-29 20:34:59 +0200

    Add type hints, handle curses input better

--- 3615-gpt-curses.py
+++ 3615-gpt-curses.py
@@ -8,42 +8,35 @@ import curses
 import sys
 from curses import wrapper
 
+from typing_extensions import Dict
+from typing import TYPE_CHECKING, Dict, Any, List
+if TYPE_CHECKING:
+    from _curses import _CursesWindow
+    Window = _CursesWindow
+else:
+    from typing import Any
+    Window = Any
 
-# Init openai
 
-LANG = 'en'
-
-strings = {
-    "fr": {
-        "preprompt": "Tu es un gentil assistant",
-        "send": "Tapez votre commande, puis ",
-        "next": "Page suivante:",
-        "back": "Revenir au début:",
-        "intro": "Interrogez ChatGPT",
-        "intro2": "sur votre Minitel!",
-        "tagline": "ChatGPT sur votre Minitel",
-    },
-    "en": {
-        "preprompt": "You are a kind assistant",
-        "send": "Type your command, then press ",
-        "next": "Next page:",
-        "back": "To go back:",
-        "intro": "Talk to ChatGPT",
-        "intro2": "on your Minitel!",
-        "tagline": "ChatGPT on your Minitel",
-    }
+strings: Dict[str, str] = {
+    "preprompt": "You are a kind assistant",
+    "send": "Type your command, then press ",
+    "next": "Next page:",
+    "back": "To go back:",
+    "intro": "Talk to ChatGPT",
+    "intro2": "on your Minitel!",
+    "tagline": "ChatGPT on your Minitel",
 }
 
 
-openai.organization = "org-SXynwmSaKFYwkwqBBnzAZO5m"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def ask_gpt(prompt):
+def ask_gpt(prompt: str) -> str:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": strings[LANG]["preprompt"]},
+            {"role": "system", "content": strings["preprompt"]},
             {"role": "user", "content": prompt},
         ],
         temperature=0,
@@ -54,19 +47,17 @@ def ask_gpt(prompt):
 
 # im = Image.open("chatgpt-logo-pixellated-1-to-1.png")
 
-NB_ROWS = 24
-NB_COLS = 40
-KEY_ENVOI = [19, 65]
-KEY_CORRECTION = [19, 71]
-KEY_RETOUR = [19, 66]
-KEY_SUITE = [19, 72]
+NB_ROWS: int = 24
+NB_COLS: int = 40
+KEY_ENVOI: List[int] = [19, 65]
+KEY_CORRECTION: List[int] = [19, 71]
+KEY_RETOUR: List[int] = [19, 66]
+KEY_SUITE: List[int] = [19, 72]
 
 
 curses.setupterm(term="vt100")
-stdscr = curses.initscr()  # initialise curses
+#stdscr = curses.initscr()  # initialise curses
 # curses.noecho()  # don't echo incoming characters
-curses.cbreak()  # don't wait for Enter to handle keypresses
-stdscr.keypad(True)  # automatically decode special keys into constants
 
 
 
@@ -79,11 +70,11 @@ stdscr.keypad(True)  # automatically decode special keys into constants
 # print(im.getpixel((2,2)))
 
 
-def pixel(col, row):
-    return 0 if im.getpixel((col, row)) != (255, 255, 255) else 1
+# def pixel(col: int, row: int):
+#    return 0 if im.getpixel((col, row)) != (255, 255, 255) else 1
 
 
-def button(row, col, text):
+def button(stdscr: Window, row: int, col: int, text: str):
     stdscr.addstr(col, row, f" {text} ", curses.A_REVERSE)
     stdscr.refresh()
 
@@ -96,28 +87,34 @@ def wait_for(keycode):
             return
 
 
-def require_input(row, col):
+def require_input(stdscr: Window, row: int, col: int):
     # Input field
 
     stdscr.addstr(row, col, "........................................")
-    stdscr.addstr(row+2, col, strings[LANG]["send"])
+    stdscr.addstr(row+2, col, strings["send"])
 
-    button(col+len(strings[LANG]["send"]), row+2, 'Envoi')
+    button(stdscr, col+len(strings["send"]), row+2, 'Envoi')
 
     input = ""
     input_pos = col
     while True:
-        c = stdscr.getch(row, input_pos)
+        key: str = stdscr.getkey(row, input_pos)
         input_pos = input_pos + 1
 
-        if c == 263:  # backspace key
+        if key[0:4] == "KEY_":
+            continue
+        elif key == "\x7f":
+            # backspace
             input_pos = input_pos - 2
             stdscr.addstr(row, input_pos, ".")
             input = input[:-1]
-        elif c == 10:  # Return key
+        elif key == "\n":
+            # Return key
             break
         else:
-            input = input + chr(c)
+            # displayable character
+            stdscr.addstr(row, input_pos - 1, key)
+            input = input + key
 
     return input
 
@@ -192,23 +189,27 @@ def envoyer_lignes(lignes, col, row):
 #############################
 
 
-def main():
+def main(stdscr: Window):
+    curses.cbreak()  # don't wait for Enter to handle keypresses
+
+    stdscr.keypad(True)  # automatically decode special keys into constants
+
     stdscr.clear()
 
     stdscr.addstr(2, 1, "3615 GPT")
 
 #    show_logo()
 
-    stdscr.addstr(10, 1, strings[LANG]["intro"])
-    stdscr.addstr(12, 1, strings[LANG]["intro2"])
+    stdscr.addstr(10, 1, strings["intro"])
+    stdscr.addstr(12, 1, strings["intro2"])
     stdscr.refresh()
 
     while True:
-        user_input = require_input(22, 1)
+        user_input = require_input(stdscr, 22, 1)
         stdscr.addstr(0, 0, f"(User typed: {user_input})                   ")
 
         if user_input == "quit":
-            sys.exit()
+            return
 
 
         #        out = ask_gpt(user_input)
@@ -220,7 +221,7 @@ def main():
         # minitel.taille(2, 2)
         # minitel.envoyer("3615 GPT")
         # minitel.position(1, 3)
-        # minitel.envoyer(strings[LANG]["tagline"])
+        # minitel.envoyer(strings["tagline"])
 
         # position_row = 6
 
@@ -231,8 +232,8 @@ def main():
 
         #     if position_row > 21:
         #         minitel.position(2, 24)
-        #         minitel.envoyer(strings[LANG]["next"])
-        #         button(3 + len(strings[LANG]["next"]), 24, 'Suite')
+        #         minitel.envoyer(strings["next"])
+        #         button(3 + len(strings["next"]), 24, 'Suite')
         #         minitel.curseur(False)
         #         wait_for(KEY_SUITE)
         #         minitel.efface()
@@ -240,7 +241,7 @@ def main():
         #         minitel.taille(2, 2)
         #         minitel.envoyer("3615 GPT")
         #         minitel.position(1, 3)
-        #         minitel.envoyer(strings[LANG]["tagline"])
+        #         minitel.envoyer(strings["tagline"])
         #         position_row = 6
 
 
@@ -250,15 +251,15 @@ def main():
         #    minitel.envoyer(out)
 
 #        minitel.position(2, 24)
-#        minitel.envoyer(strings[LANG]["back"])
-#        button(3 + len(strings[LANG]["back"]),24,'Retour')
+#        minitel.envoyer(strings["back"])
+#        button(3 + len(strings["back"]),24,'Retour')
 #        minitel.curseur(False)
 #        wait_for(KEY_RETOUR)
 
 # minitel.efface()
 
 
-wrapper(main())
+wrapper(main)
 
 
 # time.sleep(200)
